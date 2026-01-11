225a7e2664ab986e9575260841d7247d2789921c
shobhit . <shobhit.@shobhits-MacBook-Air.local>
2026-01-11 19:42:27 +0530

    12

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from Shobhit Ji!');
+  res.send('Hello from Shobhit Ji. Padhariye hamare desh va!');
 });
 
 app.listen(3001, () => console.log('App running on port 3001'));
